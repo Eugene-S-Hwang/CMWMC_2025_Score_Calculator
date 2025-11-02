7753a820ff6f92bf893516f4bb8bd009fb8d53b3
Eugene-S-Hwang <egswhwang@gmail.com>
2025-11-02 13:44:23 -0500

    testing

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 import streamlit as st
-import google.auth
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+# import google.auth
+# from googleapiclient.discovery import build
+# from googleapiclient.errors import HttpError
 
-def get_values(spreadsheet_id, range_name):
-  creds, _ = google.auth.default()
-  # pylint: disable=maybe-no-member
-  try:
-    service = build("sheets", "v4", credentials=creds)
+# def get_values(spreadsheet_id, range_name):
+#   creds, _ = google.auth.default()
+#   # pylint: disable=maybe-no-member
+#   try:
+#     service = build("sheets", "v4", credentials=creds)
 
-    result = (
-        service.spreadsheets()
-        .values()
-        .get(spreadsheetId=spreadsheet_id, range=range_name)
-        .execute()
-    )
-    rows = result.get("values", [])
-    print(f"{len(rows)} rows retrieved")
-    return result
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    return error
+#     result = (
+#         service.spreadsheets()
+#         .values()
+#         .get(spreadsheetId=spreadsheet_id, range=range_name)
+#         .execute()
+#     )
+#     rows = result.get("values", [])
+#     print(f"{len(rows)} rows retrieved")
+#     return result
+#   except HttpError as error:
+#     print(f"An error occurred: {error}")
+#     return error
 
 st.set_page_config(layout="wide")
 
